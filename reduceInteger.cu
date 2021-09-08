ee3c6a974d526e57de6924bd2314dac658758659
Maria Dalila Vieira <mdadavivi@gmail.com>
2021-09-08 14:06:10 -0300

    .

--- reduceInteger.cu
+++ reduceInteger.cu
@@ -9,53 +9,7 @@
  * are also demonstrated, such as unrolling.
  */
 
-// Recursive Implementation of Interleaved Pair Approach
-int recursiveReduce(int *data, int const size)
-{
-    // terminate check
-    if (size == 1) return data[0];
 
-    // renew the stride
-    int const stride = size / 2;
-
-    // in-place reduction
-    for (int i = 0; i < stride; i++)
-    {
-        data[i] += data[i + stride];
-    }
-
-    // call recursively
-    return recursiveReduce(data, stride);
-}
-
-// Neighbored Pair Implementation with divergence
-__global__ void reduceNeighbored (int *g_idata, int *g_odata, unsigned int n)
-{
-    // set thread ID
-    unsigned int tid = threadIdx.x;
-    unsigned int idx = blockIdx.x * blockDim.x + threadIdx.x;
-
-    // convert global data pointer to the local pointer of this block
-    int *idata = g_idata + blockIdx.x * blockDim.x;
-
-    // boundary check
-    if (idx >= n) return;
-
-    // in-place reduction in global memory
-    for (int stride = 1; stride < blockDim.x; stride *= 2)
-    {
-        if ((tid % (2 * stride)) == 0)
-        {
-            idata[tid] += idata[tid + stride];
-        }
-
-        // synchronize within threadblock
-        __syncthreads();
-    }
-
-    // write result for this block to global mem
-    if (tid == 0) g_odata[blockIdx.x] = idata[0];
-}
 /*
 // Neighbored Pair Implementation with less divergence
 __global__ void reduceNeighboredLess (int *g_idata, int *g_odata,
@@ -90,33 +44,7 @@ __global__ void reduceNeighboredLess (int *g_idata, int *g_odata,
     if (tid == 0) g_odata[blockIdx.x] = idata[0];
 }*/
 
-// Interleaved Pair Implementation with less divergence
-__global__ void reduceInterleaved (int *g_idata, int *g_odata, unsigned int n)
-{
-    // set thread ID
-    unsigned int tid = threadIdx.x;
-    unsigned int idx = blockIdx.x * blockDim.x + threadIdx.x;
 
-    // convert global data pointer to the local pointer of this block
-    int *idata = g_idata + blockIdx.x * blockDim.x;
-
-    // boundary check
-    if(idx >= n) return;
-
-    // in-place reduction in global memory
-    for (int stride = blockDim.x / 2; stride > 0; stride >>= 1)
-    {
-        if (tid < stride)
-        {
-            idata[tid] += idata[tid + stride];
-        }
-
-        __syncthreads();
-    }
-
-    // write result for this block to global mem
-    if (tid == 0) g_odata[blockIdx.x] = idata[0];
-}
 /*
 __global__ void reduceUnrolling2 (int *g_idata, int *g_odata, unsigned int n)
 {
@@ -186,46 +114,7 @@ __global__ void reduceUnrolling4 (int *g_idata, int *g_odata, unsigned int n)
 }
 */
 
-__global__ void reduceUnrolling8 (int *g_idata, int *g_odata, unsigned int n)
-{
-    // set thread ID
-    unsigned int tid = threadIdx.x;
-    unsigned int idx = blockIdx.x * blockDim.x * 8 + threadIdx.x;
 
-    // convert global data pointer to the local pointer of this block
-    int *idata = g_idata + blockIdx.x * blockDim.x * 8;
-
-    // unrolling 8
-    if (idx + 7 * blockDim.x < n)
-    {
-        int a1 = g_idata[idx];
-        int a2 = g_idata[idx + blockDim.x];
-        int a3 = g_idata[idx + 2 * blockDim.x];
-        int a4 = g_idata[idx + 3 * blockDim.x];
-        int b1 = g_idata[idx + 4 * blockDim.x];
-        int b2 = g_idata[idx + 5 * blockDim.x];
-        int b3 = g_idata[idx + 6 * blockDim.x];
-        int b4 = g_idata[idx + 7 * blockDim.x];
-        g_idata[idx] = a1 + a2 + a3 + a4 + b1 + b2 + b3 + b4;
-    }
-
-    __syncthreads();
-
-    // in-place reduction in global memory
-    for (int stride = blockDim.x / 2; stride > 0; stride >>= 1)
-    {
-        if (tid < stride)
-        {
-            idata[tid] += idata[tid + stride];
-        }
-
-        // synchronize within threadblock
-        __syncthreads();
-    }
-
-    // write result for this block to global mem
-    if (tid == 0) g_odata[blockIdx.x] = idata[0];
-}
 
 /*
 __global__ void reduceUnrollWarps8 (int *g_idata, int *g_odata, unsigned int n)
